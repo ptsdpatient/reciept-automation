c9a80342f08cc3cd624300b7c1f826da9b9dfefe
Tanishq Dhote <tanishqbakka1@gmail.com>
2024-02-14 20:14:49 +0530

    lmao

--- reciepts/main.py
+++ reciepts/main.py
@@ -1,23 +1,34 @@
 import csv
+import os
+import qrcode
 from PIL import Image, ImageDraw, ImageFont
 import random
 width, height = 640, 240
 image = Image.new("RGBA", (width, height), (255, 255, 255, 0))
 
-csv_file_path = './respawn.csv'
-
+csv_file_path = './Virtual-Placement.csv'
+eventName = 'virtual-placement'
+folderName='./'+eventName
 bg_array = []
 for i in range(0,25):
     bg_array.append("bg_{}.jpg".format(i))
 
+if not os.path.exists(folderName):
+    os.makedirs(folderName)
+def truncate_string(input_string, max_length):
+    if len(input_string) > max_length:
+        truncated_string = input_string[:max_length]
+        return truncated_string+'...'
+    else:
+        return input_string
 with open(csv_file_path, 'r') as file:
     csv_reader = csv.reader(file)
-    
-    # Skip the header row if your CSV file has one
     next(csv_reader)
     
     for row in csv_reader:
-        image_to_add_path = bg_array[random.randint(0,len(bg_array))]
+        
+        image_to_add_path = bg_array[random.randint(0,len(bg_array)-1)]
+        #image_to_add_path='bg_17.jpg'
         image_to_add = Image.open(image_to_add_path)
         image_to_add = image_to_add.resize((640, 240))
         image_position = (0, 0)
@@ -26,7 +37,7 @@ with open(csv_file_path, 'r') as file:
         image_to_add_path = "solid.png"
         image_to_add = Image.open(image_to_add_path).convert("RGBA")
         image_to_add = image_to_add.resize((150, 112))
-        image_position = (470, 110)
+        image_position = (420, 35)
         image.paste(image_to_add, image_position, image_to_add)
         
         draw = ImageDraw.Draw(image)
@@ -36,18 +47,37 @@ with open(csv_file_path, 'r') as file:
         custom_font = ImageFont.truetype(custom_font_path, 48)
         draw.text((20, 20), text, font=custom_font, fill=text_color)
         
+        # custom_font_path = "oykobold.ttf"
+        # text = 'Event Details \nDate : 20/02/2024 \nTime : 11:30 PM'
+        # text_color = "white"
+        # custom_font = ImageFont.truetype(custom_font_path, 18)
+        # draw.text((420, 35), text, font=custom_font, fill=text_color)
+        
+        #qr starts
+        
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=2.2,
+            border=4,
+        )
+        qr.add_data(f"http://example.com/authenticate?participant_id={row[0]}")
+        qr.make(fit=True)
+
+        qr_image = qr.make_image(fill_color="black", back_color="white")
+        qr_position = (460, 50)
+        image.paste(qr_image, qr_position)
+        
+        #qr end
+        
         custom_font_path = "oykobold.ttf"
-        text = 'Event Details \nDate : 20/02/2024 \nTime : 11:30 PM'
+        text = 'Participant : {} \nEmail : {}\nCollege : {} \nMobile : {}\nEvent : {} \nAmount Paid : {}'.format(row[1], row[2], truncate_string(row[4], 26),row[3],eventName, "yes")
         text_color = "white"
         custom_font = ImageFont.truetype(custom_font_path, 18)
-        draw.text((470, 35), text, font=custom_font, fill=text_color)
-        
-        
-        custom_font_path = "oykobold.ttf"
-        text = 'Participant Name : {} \nCollege : {} \nEvent : {} \nAmount Paid : {}'.format(row[1], row[2], row[3], "yes")
-        text_color = "white"
-        custom_font = ImageFont.truetype(custom_font_path, 24)
         draw.text((20, 90), text, font=custom_font, fill=text_color)
         
-        filename = '{}.png'.format(row[1])
+        filename = folderName+'/{}.png'.format(row[1])
         image.save(filename.lower())
+        
+
+
--- reciepts/trial.py
+++ reciepts/trial.py
@@ -1,3 +1,80 @@
-import random
-bg_array = ["apple", "banana", "cherry", "date"]
-print(len(bg_array))
+my_map = [{
+    "eventName": "Cricbash",
+    "Date": "20/02/24 & 21/02/24",
+    "Time": "Day 1 : 12:30-6:00\nDay 2 : 10:30-6:00 "
+},{
+    "eventName": "Avishkaar",
+    "Date":"20/02/24",
+    "Time": "12:00-2:00 ",
+    
+},{
+    "eventName": "Lounge",
+    "Date": "20/02/24 & 21/02/24",
+    "Time": "Day 1 : 2:00-4:30\nDay 2 : 12:00-3:00",
+},{
+    "eventName": "Virtual_Placement",
+    "Date": "20/02/24 & 21/02/24",
+    "Time": "Day 1 : 11:00-12:00(Paper) \n2:30-5:00(GD)\nDay 2 : 12:00-4:00"
+},{
+    "eventName": "Food-O-Holics",
+    "Date": "20/02/24 & 21/02/24",
+    "Time": "Day 1 : 1:00-6:00\nDay 2 : 1:00-6:00"
+},{
+    "eventName": "Respawn(Chess)",
+    "Date": "20/02/24",
+    "Time": "6:00-Onwards"
+},{
+    "eventName": "Respawn(Valorant)",
+    "Date": "20/02/24 & 21/02/24",
+    "Time": "Day 1 : 5:00-Onwards\nDay 2 : 6:00-Onwards"
+},{
+    "eventName": "Innovation-Express",
+    "Date": "20/02/24",
+    "Time": "3:30-5:00",
+},{
+    "eventName": "Respawn(BGMI)",
+    "Date": "21/02/24",
+    "Time": "6:00-Onwards",
+},{
+    "eventName": "Structure-Spy",
+    "Date": "21/02/24",
+    "Time": "11:00-1:00",
+},{
+    "eventName": "Stock-Talk",
+    "Date": "21/02/24",
+    "Time": "12:00-3:00",
+},{
+    "eventName": "Vaad-Vivaad",
+    "Date": "21/02/24",
+    "Time": "11:00-6:00",
+},{
+    "eventName": "Born-Psychos",
+    "Date": "21/02/24",
+    "Time": "10:30-6:00",
+},{
+    "eventName": "Codeventure",
+    "Date": "21/02/24",
+    "Time": "11:00-6:00",
+},{
+    "eventName": "Roborace",
+    "Date": "21/02/24",
+    "Time": "11:00-5:00",
+},{
+    "eventName": "Stargaze",
+    "Date": "21/02/24",
+    "Time": "5:00-7:30",
+},{
+    "eventName": "Quiz-Master",
+    "Date": "22/02/24",
+    "Time": "12:00-2:00",
+},{
+    "eventName": "GDSC-Workshop",
+    "Date": "22/02/24",
+    "Time": "12:00-2:00",
+},{
+    "eventName": "Jigyasa",
+    "Date": "22/02/24",
+    "Time": "Online",
+}]
+
+print(my_map[1].get("name"))
