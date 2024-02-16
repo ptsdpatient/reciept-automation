8b30723b302c7ad949d8ace8a562ff1a1c234943
Tanishq Dhote <tanishqbakka1@gmail.com>
2024-02-16 14:40:26 +0530

    event details fix

--- reciepts/main.py
+++ reciepts/main.py
@@ -5,7 +5,79 @@ from barcode import Code128
 from barcode.writer import ImageWriter
 from PIL import Image, ImageDraw, ImageFont
 import random
-
+eventName=''
+date=''
+time=''
+event_info = [{
+    "eventName": "Cricbash",
+    "Date": "20/02/24 & 21/02/24",
+    "Time": "Day 1 : 12:30-6:00\nDay 2 : 10:30-6:00 "
+},{
+    "eventName": "Avishkar",
+    "Date":"20/02/24",
+    "Time": "12:00-2:00 ",
+    
+},{
+    "eventName": "Lounge",
+    "Date": "20/02/24 & 21/02/24",
+    "Time": "Day 1 : 2:00-4:30\nDay 2 : 12:00-3:00",
+},{
+    "eventName": "Virtual Placement",
+    "Date": "20/02/24 & 21/02/24",
+    "Time": "Day 1 : 11:00-12:00(Paper) \n2:30-5:00(GD)\nDay 2 : 12:00-4:00"
+},{
+    "eventName": "Foodoholic",
+    "Date": "20/02/24 & 21/02/24",
+    "Time": "Day 1 : 1:00-6:00\nDay 2 : 1:00-6:00"
+},{
+    "eventName": "Respawn",
+    "Date": "20/02/24",
+    "Time": "6:00-Onwards"
+},{
+    "eventName": "Innovation-Express",
+    "Date": "20/02/24",
+    "Time": "3:30-5:00",
+},{
+    "eventName": "Structure-Spy",
+    "Date": "21/02/24",
+    "Time": "11:00-1:00",
+},{
+    "eventName": "Stock-Talk",
+    "Date": "21/02/24",
+    "Time": "12:00-3:00",
+},{
+    "eventName": "VaadVivaad",
+    "Date": "21/02/24",
+    "Time": "11:00-6:00",
+},{
+    "eventName": "Born-Psychos",
+    "Date": "21/02/24",
+    "Time": "10:30-6:00",
+},{
+    "eventName": "Codeventure",
+    "Date": "21/02/24",
+    "Time": "11:00-6:00",
+},{
+    "eventName": "Roborace",
+    "Date": "21/02/24",
+    "Time": "11:00-5:00",
+},{
+    "eventName": "Stargaze",
+    "Date": "21/02/24",
+    "Time": "5:00-7:30",
+},{
+    "eventName": "Quiz Master",
+    "Date": "22/02/24",
+    "Time": "12:00-2:00",
+},{
+    "eventName": "GDSC TF workshop",
+    "Date": "22/02/24",
+    "Time": "12:00-2:00",
+},{
+    "eventName": "Jigyasa",
+    "Date": "22/02/24",
+    "Time": "Online",
+}]
 
 width, height = 640, 240
 image = Image.new("RGBA", (width, height), (255, 255, 255, 0))
@@ -46,6 +118,10 @@ for csv_file in csv_files:
             csv_reader = csv.reader(file)
             next(csv_reader)
             for row in csv_reader:
+                for i in range (0,len(event_info)):
+                    if event_info[i].get("eventName")==file_name:
+                        date=event_info[i].get("Date")
+                        time=event_info[i].get("Time")
                 image_to_add_path = bg_array[random.randint(0,len(bg_array)-1)]
                 image_to_add = Image.open(image_to_add_path)
                 image_to_add = image_to_add.resize((640, 240))
@@ -65,14 +141,11 @@ for csv_file in csv_files:
                 custom_font = ImageFont.truetype(custom_font_path, 48)
                 draw.text((20, 20), text, font=custom_font, fill=text_color)
                 
-                
-                
-                #eventDetails
                 custom_font_path = "oykobold.ttf"
-                text = 'Event Details \nDate : 20/02/2024 \nTime : 11:30 PM'
+                text = 'Event Details \nDate : {} \nTime : {}'.format(date,time)
                 text_color = "white"
-                custom_font = ImageFont.truetype(custom_font_path, 13)
-                draw.text((405, 30), text, font=custom_font, fill=text_color)
+                custom_font = ImageFont.truetype(custom_font_path, 9)
+                draw.text((410, 35), text, font=custom_font, fill=text_color)
                 
                 #qr starts
                 
